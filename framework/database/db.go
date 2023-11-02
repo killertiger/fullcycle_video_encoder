568d7fb08886361ca6cdbf2e017e8481494a8468
moliveira <moliveira@pinterest.com>
2023-11-02 04:09:46 -0700

    feat: :sparkles: Add specification do Entities to work with gorm

--- framework/database/db.go
+++ framework/database/db.go
@@ -4,7 +4,9 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/killertiger/fullcycle_video_encoder/domain"
+	_ "github.com/lib/pq"
 )
 
 type Database struct {
@@ -59,6 +61,7 @@ func (d *Database) Connect() (*gorm.DB, error) {
 
 	if d.AutoMigrateDb {
 		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+		d.Db.Model(domain.Job{}).AddForeignKey("video_id", "videos (id)", "CASCADE", "CASCADE")
 	}
 
 	return d.Db, nil
