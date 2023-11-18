cd15eb31fc5e111643ba8f3bddf0613609c3c763
moliveira <moliveira@pinterest.com>
2023-11-18 05:50:24 -0800

    feat: :sparkles: Create the JobService

---
+++ application/services/job_service.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"github.com/killertiger/fullcycle_video_encoder/application/repositories"
+	"github.com/killertiger/fullcycle_video_encoder/domain"
+)
+
+type JobService struct {
+	Job           *domain.Job
+	JobRepository repositories.JobRepository
+	VideoService  VideoService
+}
+
+func (j *JobService) Start() error {
+
+}
+
+func (j *JobService) changeJobStatus(status string) error {
+
+	var err error
+
+	j.Job.Status = status
+	j.Job, err = j.JobRepository.Update(j.Job)
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	return nil
+}
+
+func (j *JobService) failJob(error error) error {
+
+	j.Job.Status = "FAILED"
+	j.Job.Error = error.Error()
+
+	_, err := j.JobRepository.Update(j.Job)
+
+	if err != nil {
+		return err
+	}
+
+	return error
+}
